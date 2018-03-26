0d124ea4f05ac7e871dfedf113bde04c90deda69
Guilherme Poltronieri <gpmmoreno@gmail.com>
2018-03-26 00:49:51 -0300

    adiciona logica para o teste de compressão de dados

--- js/compression/compression.js
+++ js/compression/compression.js
@@ -1,5 +1,40 @@
 const compression = () => {
-  console.log('hahaha');
+  let input = '5\naaa3215555saas2344\n332122122222aa\naaaaaaa2sssss3\n454333211\naqsaqws5541';
+  input.split('\n').forEach(line => {
+    let previousChar = '';
+    let currentChar = '';
+    let nextChar = '';
+    let repeats = 1;
+    let result = '';
+    if (line.length > 1) {
+      let lineByChar = line.split('');
+      for (let position in lineByChar) {
+        position = parseInt(position);
+        currentChar = line[position];
+        nextChar = line[position + 1] ? line[position + 1] : 'end';
+        if (position === 0) {
+          previousChar = currentChar;
+        } else {
+          if (currentChar == previousChar) {
+            repeats++;
+          } else if (currentChar == nextChar) {
+            if (repeats > 1) {
+              result = result.concat(`{${previousChar},${repeats}}`);
+            }
+          } else {
+            if (repeats > 1) {
+              result = result.concat(`{${previousChar},${repeats}}`);
+              repeats = 1;
+            }
+            result = result.concat(currentChar);
+          }
+          previousChar = currentChar;
+        }
+      }
+      console.log(result);
+      console.log(line);
+    }
+  });
 };
 
 export default compression;
